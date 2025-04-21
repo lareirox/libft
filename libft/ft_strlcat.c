8dc6a1d470baf1c4a4d8fb302fe7c7bda28c97b3
nugoncal <nugoncal@42porto.student.com>
2025-04-21 12:27:43 +0100

    1st

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nugoncal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 18:00:30 by nugoncal          #+#    #+#             */
+/*   Updated: 2025/04/17 10:56:08 by nugoncal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, const char *src, unsigned int destsize)
+{
+	unsigned int	i;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+
+	dest_len = 0;
+	while (dest[dest_len] && (dest_len < destsize))
+		dest_len++;
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
+	if (destsize <= dest_len)
+		return (destsize + src_len);
+	else
+	{
+		i = 0;
+		while (src[i] && (dest_len + i + 1 < destsize))
+		{
+			dest[dest_len + i] = src[i];
+			i++;
+		}
+		dest[dest_len + i] = '\0';
+	}
+	return (dest_len + src_len);
+}
